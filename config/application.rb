de4672dac3bca2703a7fd1802fd36e96217dedf8
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2024-06-02 12:46:16 +0900

    update

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,11 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require './lib/CloudflareProxy'
+
+config.middleware.use CloudflareProxy
+
+
 module SecondApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
