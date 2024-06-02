09c1c6fbfd9c6a456fabe127dc63c78d5ebf8c55
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2024-06-02 12:36:25 +0900

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  config.action_controller.default_url_options = { host: 'kkappservice.com', protocol: 'https' }
   
   before_action :configure_permitted_parameters, if: :devise_controller?
 
